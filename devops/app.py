cd44a5733482d90c985e1042c3ccb298ab9208f5
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-30 21:17:41 -0500

    import VPCStack in app.py

--- devops/app.py
+++ devops/app.py
@@ -1,6 +1,9 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 import aws_cdk as cdk
+from stacks.vpc_stack import VPCStack
 
 app = cdk.App()
 
-app.synth
+vpc_stack = VPCStack(app, 'vpc')
+
+app.synth()
