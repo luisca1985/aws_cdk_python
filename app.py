99ef9782505c36ba091d3fd187947520970f592b
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-31 23:05:28 -0500

    include bastion stack configuration

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 import aws_cdk as cdk
 from stacks.vpc_stack import VPCStack
 from stacks.security_stack import SecurityStack
+from stacks.bastion_stack import BastionStack
 
 app = cdk.App()
 
 vpc_stack = VPCStack(app, 'vpc')
 security_stack = SecurityStack(app, 'security-stack', vpc=vpc_stack.vpc)
+bastion_stack = BastionStack(app, 'bastion', vpc=vpc_stack.vpc,sg=security_stack.bastion_sg)
 
 app.synth()
