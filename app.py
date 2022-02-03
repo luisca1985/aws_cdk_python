f82370f9f2ac233bb6abb0b2a91656c4869e15d2
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-02-02 19:40:28 -0500

    Task 1 - Configure VPC

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
 import aws_cdk as cdk
 from stacks.vpc_stack import VPCStack
-from stacks.security_stack import SecurityStack
-from stacks.bastion_stack import BastionStack
-from stacks.kms_stack import KMSStack
 
 app = cdk.App()
 
-vpc_stack = VPCStack(app, 'vpc')
-security_stack = SecurityStack(app, 'security-stack', vpc=vpc_stack.vpc)
-bastion_stack = BastionStack(app, 'bastion', vpc=vpc_stack.vpc,sg=security_stack.bastion_sg)
-kms_stack = KMSStack(app, 'kms')
+vpc_stack = VPCStack(app, 'task1vpc')
 
 app.synth()
